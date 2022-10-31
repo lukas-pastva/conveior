5d269d6dda96ed98d0a6858e56d04afac31ba7b7
lukas-pastva <info@lukaspastva.sk>
2022-10-31 21:38:57 +0100

    Initial commit

--- conveior-image/action/sys-entrypoint.sh
+++ conveior-image/action/sys-entrypoint.sh
@@ -10,13 +10,14 @@ echo " ██████  ██████  ██   ████   ███
 echo ""
 echo ""
 
-service cron start & tail -f /var/log/cron.log
 
 #nginx -g "daemon off;"
 
-#while true;
-#  do echo -e "HTTP/1.1 200 OK\n\n$(bash /action/sys-cron.sh)" \
-#  | nc -l -k -p 8080 -q 1;
-#done
+while true;
+  do echo -e "HTTP/1.1 200 OK\n\n$(bash /action/sys-cron.sh)" \
+  | nc -l -k -p 8080 -q 1;
+done
 
-echo -e "HTTP/1.1 200 OK\r\n$(date)\r\n\r\n$(bash /action/sys-cron.sh)" |  nc -vl 8080
+#echo -e "HTTP/1.1 200 OK\r\n$(date)\r\n\r\n$(bash /action/sys-cron.sh)" |  nc -vl 8080
+
+#service cron start & tail -f /var/log/cron.log
