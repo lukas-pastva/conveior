64d52813c8ecfecd8327e275894095a545798773
lukas-pastva <info@lukaspastva.sk>
2022-10-31 20:18:47 +0100

    Initial commit

--- conveior-image/action/sys-cron.sh
+++ conveior-image/action/sys-cron.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export $(xargs -0 -a "/proc/1/environ")
 
+echo -ne "HTTP/1.0 200 OK\r\nContent-Length: 1024\r\n\r\n"
+
 bash /action/action-hw.sh
 bash /action/action-url.sh
 bash /action/action-container.sh
