165453cadd65231ce055e1d3ee6955c29cf5ec88
lukas-pastva <info@lukaspastva.sk>
2022-11-01 11:49:32 +0100

    Initial commit

--- conveior-image/action/sys-cron.sh
+++ conveior-image/action/sys-cron.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export $(xargs -0 -a "/proc/1/environ")
 
-echo "asdf"
+echo "# conveior.io return data"
+bash /action/action-hw.sh
 #return=$(bash /action/action-hw.sh)
 #echo -e "\r\nContent-type: text/html\r\n\r\n${return}"
 
--- conveior-image/action/sys-entrypoint.sh
+++ conveior-image/action/sys-entrypoint.sh
@@ -15,10 +15,11 @@ nginx -g "daemon off;"
 
 #  do echo -e "HTTP/1.1 200 OK\r\n$(date)\r\nContent-type: text/html\r\n\r\n$(bash /action/sys-cron.sh)" | nc -l -k -q 5 -p 8080 -q 1;
 
-
+echo "Starting NetCat server..."
 while true; do { \
   echo -ne "HTTP/1.1 200 OK\r\n"; bash /action/sys-cron.sh; } | nc -l -k -q 2 8080; \
 done
+echo "started"
 
 #echo -e "HTTP/1.1 200 OK\r\n$(date)\r\n\r\n$(bash /action/sys-cron.sh)" |  nc -vl 8080
 
