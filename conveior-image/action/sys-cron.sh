e4d2227e6158d9f2b78a7b8ffb4192089ebd1fd6
lukas-pastva <info@lukaspastva.sk>
2022-11-01 09:29:14 +0100

    Initial commit

--- conveior-image/action/sys-cron.sh
+++ conveior-image/action/sys-cron.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 export $(xargs -0 -a "/proc/1/environ")
 
-echo -e "HTTP/1.0 200 OK\r\n$(date)\r\n\r\n"
 
-bash /action/action-hw.sh
-bash /action/action-url.sh
-bash /action/action-container.sh
-bash /action/action-php.sh
-bash /action/action-apache.sh
-bash /action/action-mysql.sh
-bash /action/action-mysql-query.sh
-bash /action/action-pgsql-query.sh
+return=$(bash /action/action-hw.sh)
+echo -e "\r\nContent-type: text/html\r\n\r\n${return}"
+
+#echo -e "HTTP/1.0 200 OK\r\n$(date)\r\nContent-type: text/html\r\n\r\n${return}"
+
+#bash /action/action-url.sh
+#bash /action/action-container.sh
+#bash /action/action-php.sh
+#bash /action/action-apache.sh
+#bash /action/action-mysql.sh
+#bash /action/action-mysql-query.sh
+#bash /action/action-pgsql-query.sh
