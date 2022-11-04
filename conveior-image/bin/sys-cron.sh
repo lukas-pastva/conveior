12087cf74bb9b3e1ed770370633ee76b485a2b7f
lukas-pastva <info@lukaspastva.sk>
2022-11-04 07:39:24 +0100

    Initial commit

--- conveior-image/bin/sys-cron.sh
+++ conveior-image/bin/sys-cron.sh
@@ -2,18 +2,9 @@
 export $(xargs -0 -a "/proc/1/environ")
 
 echo "# conveior.io return data"
-bash metrics-mysql-query.sh
+bash metrics-mysql.sh
 
 #return=$(bash metrics-hw.sh)
 #echo -e "\r\nContent-type: text/html\r\n\r\n${return}"
 
 #echo -e "HTTP/1.0 200 OK\r\n$(date)\r\nContent-type: text/html\r\n\r\n${return}"
-
-#bash metrics-hw.sh
-#bash metrics-url.sh
-#bash metrics-container.sh
-#bash metrics-php.sh
-#bash metrics-apache.sh
-#bash metrics-mysql.sh
-#bash metrics-mysql-query.sh
-#bash metrics-pgsql-query.sh
