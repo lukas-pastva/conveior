1b509fa67c687165bfa5a2bdb30c543eed168bab
lukas-pastva <info@lukaspastva.sk>
2022-11-24 18:11:55 +0100

    First official version

--- conveior-image/bin/backup-files.sh
+++ conveior-image/bin/backup-files.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 source functions.inc.sh
 
-while read ITEM;
+while read POD_SHORT;
 do
-  export POD_SHORT=$(echo ${ITEM} | awk -F":" '{print $1}')
   get_pod_name "${POD_SHORT}"
   POD="${func_result}"
   if [[ "${POD}" != "" ]]; then
     echo_prom_helper "Backing up $ITEM"
 
-    export VOLUME=$(echo ${ITEM} | awk -F":" '{print $2}')
+    export PATH=$(yq e ".backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .0.path" /home/conveior-config.yaml)
     export SERVER_DIR="/tmp/${POD_SHORT}"
     export FILE="${POD_SHORT}-${DATE}"
     export ZIP_FILE_ONLY="${FILE}.zip"
     export ZIP_FILE="${SERVER_DIR}/${ZIP_FILE_ONLY}"
 
+    echo "mkdir -p \"${SERVER_DIR}\""
     mkdir -p "${SERVER_DIR}"
     find "${SERVER_DIR}" -mindepth 1 -delete
 
-    if [[ $(docker exec -i "${POD}" bash -c "cd ${VOLUME} && ls | wc -l") != "0" ]]; then
-      docker cp ${POD}:${VOLUME} ${SERVER_DIR}
-      export LAST_DIR=$(echo ${VOLUME} | awk -F"/" '{print $NF}')
+    if [[ $(docker exec -i "${POD}" bash -c "cd ${PATH} && ls | wc -l") != "0" ]]; then
+      docker cp ${POD}:${PATH} ${SERVER_DIR}
+      export LAST_DIR=$(echo ${PATH} | awk -F"/" '{print $NF}')
       cd "${SERVER_DIR}/${LAST_DIR}" && zip -rqq "${ZIP_FILE}" "."
 
       upload_file "${ZIP_FILE}" "backup-file/${POD_SHORT}/${ZIP_FILE_ONLY}"
       find "${SERVER_DIR}" -mindepth 1 -delete
     else
-      echo_prom_helper "Empty directory ${VOLUME}, nothing to backup"
+      echo_prom_helper "Empty directory ${PATH}, nothing to backup"
     fi
   fi
 done < <(yq e '.backups.files.[].name' /home/conveior-config.yaml)
