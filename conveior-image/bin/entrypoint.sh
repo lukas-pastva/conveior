967114c3611afc08958e35fe14491222196344a8
lukas-pastva <info@lukaspastva.sk>
2022-11-27 18:11:28 +0100

    First official version

--- conveior-image/bin/entrypoint.sh
+++ conveior-image/bin/entrypoint.sh
@@ -12,11 +12,10 @@ echo ""
 
 #nginx -g "daemon off;"
 #  do echo -e "HTTP/1.1 200 OK\r\n$(date)\r\nContent-type: text/html\r\n\r\n$(bash /usr/local/bin/metrics.sh)" | nc -l -k -q 5 -p 8080 -q 1;
-
+service cron start & tail -f /var/log/cron.log
 echo "Starting NetCat server..."
 while true; do { \
   echo "HTTP/1.1 200 OK"; echo ""; bash /usr/local/bin/metrics.sh; } | nc -l -k -q 2 8080; \
 done
 #echo -e "HTTP/1.1 200 OK\r\n$(date)\r\n\r\n$(bash /usr/local/bin/metrics.sh)" |  nc -vl 8080
 
-service cron start & tail -f /var/log/cron.log
