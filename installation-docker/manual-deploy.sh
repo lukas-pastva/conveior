511f908e9ee506e48dd5358c8c47a8baa541156d
lukas-pastva <info@lukaspastva.sk>
2022-12-27 22:43:27 +0100

    Added DB custom username/password

--- installation-docker/manual-deploy.sh
+++ installation-docker/manual-deploy.sh
@@ -5,19 +5,24 @@ echo "Manually deploying Conveior into Docker."
 
 . .env 2>/dev/null || true
 
-if [ -z "${CONVEIOR_S3_KEY+xxx}" ]; then
-  read -p "Please input CONVEIOR_S3_KEY (example: *****) " CONVEIOR_S3_KEY
-  export CONVEIOR_S3_KEY="${CONVEIOR_S3_KEY}"
+if [ -z "${S3_KEY+xxx}" ]; then
+  read -p "Please input S3_KEY (example: *****) " S3_KEY
+  export S3_KEY="${S3_KEY}"
 fi
 
-if [ -z "${CONVEIOR_S3_SECRET+xxx}" ]; then
-  read -p "Please input CONVEIOR_S3_SECRET (example: *****) " CONVEIOR_S3_SECRET
-  export CONVEIOR_S3_SECRET="${CONVEIOR_S3_SECRET}"
+if [ -z "${S3_SECRET+xxx}" ]; then
+  read -p "Please input S3_SECRET (example: *****) " S3_SECRET
+  export S3_SECRET="${S3_SECRET}"
 fi
 
-if [ -z "${CONVEIOR_S3_URL+xxx}" ]; then
-  read -p "Please input CONVEIOR_S3_URL (example: https://eu2.contabostorage.com) " CONVEIOR_S3_URL
-  export CONVEIOR_S3_URL="${CONVEIOR_S3_URL}"
+if [ -z "${S3_URL+xxx}" ]; then
+  read -p "Please input S3_URL (example: https://eu2.contabostorage.com) " S3_URL
+  export S3_URL="${S3_URL}"
 fi
 
-docker-compose -f compose-deploy.yaml up -d
+export IMAGE="lukaspastva/conveior:latest"
+
+docker stop conveior || true
+docker container rm conveior || true
+docker image rm ${IMAGE} || true
+docker run --name conveior -e "CONVEIOR_S3_KEY=${S3_KEY}" -e "CONVEIOR_S3_SECRET=${S3_SECRET}" -e "CONVEIOR_S3_URL=${S3_URL}" -d -v /var/run/docker.sock:/var/run/docker.sock  -v ./conveior-config.yaml:/home/conveior-config.yaml ${IMAGE} .
