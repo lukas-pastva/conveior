dcc7523e05edbb9ba7642c8fae36c3d9370124bb
Lukas Pastva <lukas.pastva>
2023-12-10 23:24:16 +0100

    feat: add forwarder

--- source-code/conveior/bin/metrics-forwarder.sh
+++ source-code/conveior/bin/metrics-forwarder.sh
@@ -4,11 +4,10 @@ source functions.inc.sh
 while read CONTAINER;
 do
   export PUSH_GW_URL=$(yq e ".config.forwarder | with_entries(select(.value.name == \"$CONTAINER\")) | .[].pushGw" ${CONFIG_FILE_DIR})
+  export METRICS=$(curl -sk $CONTAINER)
+  METRICS="${METRICS}
+conveior_hwHeartbeat{container=\"$CONTAINER\"} $(date +%s)"
 
-  # export DATA=$(curl -ik CONTAINER)
-  # METRIC="conveior_dockerContainer{label_name=\"${CONTAINER}\"} ${VALUE}"
-  # METRICS=$(echo -e "$METRICS\n$METRIC")
-  # echo -e "$METRICS" | curl --data-binary @- "${GW_URL}"
+  echo -e "$METRICS" | curl --data-binary @- "${PUSH_GW_URL}"
 
 done < <(yq e ".config.forwarder | .[].name" ${CONFIG_FILE_DIR})
-
