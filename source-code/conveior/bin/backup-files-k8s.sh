acf062aa0fba960a43e5ef0df0f8855c7f51a322
Lukas Pastva <lukas.pastva>
2023-12-20 17:30:44 +0100

    feat: add forwarder

--- source-code/conveior/bin/backup-files-k8s.sh
+++ source-code/conveior/bin/backup-files-k8s.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 source functions.inc.sh
 
-export PODS=$(yq e '.config.backups.files.[].name' ${CONFIG_FILE_DIR})
-export IFS=$'\n'
+PODS=$(yq e '.config.backups.files.[].name' ${CONFIG_FILE_DIR})
+IFS=$'\n'
 for POD_SHORT in $PODS;
 do
   echo_message "Backing up $POD_SHORT"
 
-  export POD_NAMESPACE=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].namespace" ${CONFIG_FILE_DIR})
-  export POD=$(eval "kubectl -n ${POD_NAMESPACE} get pods --no-headers -o custom-columns=\":metadata.name\" | grep ${POD_SHORT}")
-  export POD_PATH=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].path" ${CONFIG_FILE_DIR})
+  POD_NAMESPACE=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].namespace" ${CONFIG_FILE_DIR})
+  POD=$(eval "kubectl -n ${POD_NAMESPACE} get pods --no-headers -o custom-columns=\":metadata.name\" | grep ${POD_SHORT}")
+  POD_PATH=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].path" ${CONFIG_FILE_DIR})
 
-  export SERVER_DIR="/tmp/${POD_SHORT}"
-  export ZIP_FILE_ONLY="${POD_SHORT}-${DATE}.zip"
-  export ZIP_FILE="${SERVER_DIR}/${ZIP_FILE_ONLY}"
+  SERVER_DIR="/tmp/${POD_SHORT}"
+  ZIP_FILE_ONLY="${POD_SHORT}-${DATE}.zip"
+  ZIP_FILE="${SERVER_DIR}/${ZIP_FILE_ONLY}"
 
   mkdir -p "${SERVER_DIR}"
   find "${SERVER_DIR}" -mindepth 1 -delete
