2104dfe47699a8ce6a527ee9cca5e950355de762
Lukas Pastva <lukas.pastva>
2023-12-21 08:28:14 +0100

    feat: add forwarder

--- source-code/conveior/bin/backup-files-k8s.sh
+++ source-code/conveior/bin/backup-files-k8s.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 source functions.inc.sh
 
-PODS=$(yq e '.config.backups.files.[].name' ${CONFIG_FILE_DIR})
+PODS=$(yq e '.config.backups.files.[].name' "${CONFIG_FILE_DIR}")
 IFS=$'\n'
 for POD_SHORT in $PODS;
 do
   echo_message "Backing up $POD_SHORT"
 
-  POD_NAMESPACE=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].namespace" ${CONFIG_FILE_DIR})
-  POD=$(eval "kubectl -n ${POD_NAMESPACE} get pods --no-headers -o custom-columns=\":metadata.name\" | grep ${POD_SHORT}")
-  POD_PATH=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].path" ${CONFIG_FILE_DIR})
+  POD_NAMESPACE=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].namespace" "${CONFIG_FILE_DIR}")
+  POD_REGEX="^${POD_SHORT}(-[a-z0-9]{10}-[a-z0-9]{5})?|${POD_SHORT}-[0-9]+$"
+  POD=$(kubectl -n "${POD_NAMESPACE}" get pods --no-headers -o custom-columns=":metadata.name" | grep -E "${POD_REGEX}" | head -n 1)
+  POD_PATH=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].path" "${CONFIG_FILE_DIR}")
 
   SERVER_DIR="/tmp/${POD_SHORT}"
   ZIP_FILE_ONLY="${POD_SHORT}-${DATE}.zip"
-  ZIP_FILE="${SERVER_DIR}/${ZIP_FILE_ONLY}"
 
-  mkdir -p "${SERVER_DIR}"
-  find "${SERVER_DIR}" -mindepth 1 -delete
+  mkdir -p "${SERVER_DIR}" && find "${SERVER_DIR}" -mindepth 1 -delete
 
-  if [[ $(eval "kubectl -n ${POD_NAMESPACE} exec -i ${POD} -- sh -c \"cd ${POD_PATH} && ls | wc -l\"") != "0" ]]; then
-    eval "kubectl -n ${POD_NAMESPACE} cp ${POD}:${POD_PATH} ${SERVER_DIR}"
-    cd "${SERVER_DIR}" && zip -rqq "${ZIP_FILE}" "."
+  if [[ $(kubectl -n "${POD_NAMESPACE}" exec -i "${POD}" -- sh -c "cd ${POD_PATH} && ls | wc -l") != "0" ]]; then
+    kubectl -n "${POD_NAMESPACE}" cp "${POD}:${POD_PATH}" "${SERVER_DIR}" >/dev/null
+    cd "${SERVER_DIR}"
+    zip -rqq "${ZIP_FILE_ONLY}" "."
 
-    upload_file "${ZIP_FILE}" "backup-file/${POD_SHORT}/${ZIP_FILE_ONLY}"
+    upload_file "${SERVER_DIR}/${ZIP_FILE_ONLY}" "backup-file/${POD_SHORT}/${ZIP_FILE_ONLY}"
     find "${SERVER_DIR}" -mindepth 1 -delete
   fi
 
--- source-code/conveior/bin/backup-mysql-k8s.sh
+++ source-code/conveior/bin/backup-mysql-k8s.sh
@@ -1,71 +1,67 @@
 #!/bin/bash
 source functions.inc.sh
 
-export POD_SHORT_LIST=$(yq e '.config.backups.dbs_mysql.[].name' ${CONFIG_FILE_DIR})
-export IFS=$'\n'
+POD_SHORT_LIST=$(yq e '.config.backups.dbs_mysql.[].name' "${CONFIG_FILE_DIR}")
+IFS=$'\n'
 for POD_SHORT in $POD_SHORT_LIST;
 do
   echo_message "Backing up ${POD_SHORT}"
 
-  export POD_NAMESPACE=$(yq e ".config.backups.dbs_mysql | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].namespace" ${CONFIG_FILE_DIR})
-  export POD_LIST=$(eval "kubectl -n ${POD_NAMESPACE} get pods --no-headers -o custom-columns=\":metadata.name\" | grep ${POD_SHORT}")
-
+  POD_NAMESPACE=$(yq e ".config.backups.dbs_mysql | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].namespace" "${CONFIG_FILE_DIR}")
+  
+  POD_REGEX="^${POD_SHORT}-([a-z0-9]+-[a-z0-9]+|[0-9]+)$"
+  POD_LIST=$(kubectl -n "${POD_NAMESPACE}" get pods --no-headers -o custom-columns=":metadata.name" | grep -E "${POD_REGEX}" | head -n 1)
   for POD in $POD_LIST;
   do
-    export DATABASES_STR=""
-    export SERVER_DIR="/tmp/${POD_SHORT}"
-    export FILE="${POD_SHORT}-${DATE}.sql"
-    export DESTINATION_FILE="${SERVER_DIR}/${FILE}.gz"
+    DATABASES_STR=""
+    SERVER_DIR="/tmp/${POD_SHORT}"
+    FILE="${POD_SHORT}-${DATE}.sql"
+    DESTINATION_FILE="${SERVER_DIR}/${FILE}.gz"
 
     # try to get username from config
-    export SQL_USER=$(yq e ".config.backups.dbs_mysql | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].username" ${CONFIG_FILE_DIR})
+    SQL_USER=$(yq e ".config.backups.dbs_mysql | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].username" "${CONFIG_FILE_DIR}")
     if [[ "${SQL_USER}" == "null" ]]; then
-      export SQL_USER="root"
+      SQL_USER="root"
     fi
 
     # try to get password from config
-    export SQL_PASS=$(yq e ".config.backups.dbs_mysql | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].password" ${CONFIG_FILE_DIR})
+    SQL_PASS=$(yq e ".config.backups.dbs_mysql | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].password" "${CONFIG_FILE_DIR}")
     if [[ "${SQL_PASS}" == "null" ]]; then
-      export SQL_PASS=$(eval "kubectl -n ${POD_NAMESPACE} exec -i ${POD} -- bash -c 'echo \${MYSQL_ROOT_PASSWORD}'")
+      SQL_PASS=$(kubectl -n "${POD_NAMESPACE}" exec -i "${POD}" -- bash -c 'echo ${MYSQL_ROOT_PASSWORD}')
     fi
-    echo "POD: $POD"
 
     mkdir -p "${SERVER_DIR}"
     find "${SERVER_DIR}" -mindepth 1 -delete
 
-    export DATABASE_ITEMS=$(eval "kubectl -n ${POD_NAMESPACE} exec -i ${POD} -- mysql -u ${SQL_USER} -p'${SQL_PASS}' -e \"show databases;\" 2>/dev/null" )
-    export IFS=$'\n'
+    DATABASE_ITEMS=$(kubectl -n "${POD_NAMESPACE}" exec -i "${POD}" -- bash -c "mysql -u '${SQL_USER}' -p'${SQL_PASS}' -e 'show databases;' 2>/dev/null")
+    IFS=$'\n'
     for DATABASE_ITEM in $DATABASE_ITEMS;
     do
-      if [[ "${DATABASE_ITEM}" != "Database" ]]; then
-        if [[ "${DATABASE_ITEM}" != "information_schema" ]]; then
-          if [[ "${DATABASE_ITEM}" != "mysql" ]]; then
-            if [[ "${DATABASE_ITEM}" != "performance_schema" ]]; then
-              if [[ "${DATABASE_ITEM}" != "sys" ]]; then
-                DATABASES_STR="${DATABASE_ITEM} ${DATABASES_STR}"
-              fi
-            fi
-          fi
-        fi
+      if [[ "${DATABASE_ITEM}" != "Database" ]] && \
+         [[ "${DATABASE_ITEM}" != "information_schema" ]] && \
+         [[ "${DATABASE_ITEM}" != "mysql" ]] && \
+         [[ "${DATABASE_ITEM}" != "performance_schema" ]] && \
+         [[ "${DATABASE_ITEM}" != "sys" ]]; then
+        DATABASES_STR="${DATABASE_ITEM} ${DATABASES_STR}"
       fi
     done
-    echo_message "Fount DBs: ${DATABASES_STR}"
+    echo_message "Found DBs: ${DATABASES_STR}"
     if [[ "${DATABASES_STR}" != "" ]]; then
-      eval "kubectl -n ${POD_NAMESPACE} exec -i ${POD} -- bash -c \"mysqldump --user=${SQL_USER} --password='${SQL_PASS}' --extended-insert --databases ${DATABASES_STR} > /tmp/${FILE} 2>/dev/null\""
-      eval "kubectl cp ${POD_NAMESPACE}/${POD}:/tmp/${FILE} ${SERVER_DIR}/${FILE}"
-      eval "kubectl -n ${POD_NAMESPACE} exec -i ${POD} -- bash -c \"rm /tmp/${FILE}\""
+      kubectl -n "${POD_NAMESPACE}" exec -i "${POD}" -- bash -c "mysqldump --user=${SQL_USER} --password='${SQL_PASS}' --extended-insert --databases ${DATABASES_STR} > /tmp/${FILE} 2>/dev/null"
+      kubectl cp "${POD_NAMESPACE}/${POD}:/tmp/${FILE}" "${SERVER_DIR}/${FILE}" >/dev/null
+      kubectl -n "${POD_NAMESPACE}" exec -i "${POD}" -- bash -c "rm /tmp/${FILE}"
 
-      export ZIP_FILE_ONLY="${FILE}.zip"
-      export ZIP_FILE="${SERVER_DIR}/${ZIP_FILE_ONLY}"
+      ZIP_FILE_ONLY="${FILE}.zip"
+      ZIP_FILE="${SERVER_DIR}/${ZIP_FILE_ONLY}"
 
-      ENCRYPT=$(yq e ".config.backups.dbs_mysql | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].encrypt" ${CONFIG_FILE_DIR})
+      ENCRYPT=$(yq e ".config.backups.dbs_mysql | with_entries(select(.value.name == \"$POD_SHORT\")) | .[].encrypt" "${CONFIG_FILE_DIR}")
       if [ "${ENCRYPT}" == "true" ]; then
         zip -qq --password "${SQL_PASS}" "${ZIP_FILE}" "${SERVER_DIR}/${FILE}"
       else
-        zip -qq "${ZIP_FILE}" "/${SERVER_DIR}/${FILE}"
+        zip -qq "${ZIP_FILE}" "${SERVER_DIR}/${FILE}"
       fi
 
-      rm "/${SERVER_DIR}/${FILE}"
+      rm "${SERVER_DIR}/${FILE}"
       upload_file "${ZIP_FILE}" "backup-mysql/${POD_SHORT}/${ZIP_FILE_ONLY}"
 
       rm "${ZIP_FILE}"
