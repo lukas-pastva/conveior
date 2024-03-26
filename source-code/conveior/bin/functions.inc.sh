f224227860be335b11fe55cfa3299ea6ae5be470
Lukas Pastva <lukas.pastva>
2024-03-26 10:26:09 +0100

    feat: rclone

--- source-code/conveior/bin/functions.inc.sh
+++ source-code/conveior/bin/functions.inc.sh
@@ -174,4 +174,3 @@ export S3_URL=$(yq e '.config.s3_url' ${CONFIG_FILE_DIR}) || true
 export S3_KEY=$(yq e '.config.s3_key' ${CONFIG_FILE_DIR}) || true
 export S3_SECRET=$(yq e '.config.s3_secret' ${CONFIG_FILE_DIR}) || true
 export CONTAINER_ORCHESTRATOR=$(yq e '.config.container_orchestrator' ${CONFIG_FILE_DIR})
-
