f839367de1f8fdc1a3f267d46228ed898c9fff32
Lukas Pastva <lukas.pastva>
2024-04-19 17:24:47 +0200

    feat: fix forwarder

--- source-code/conveior/bin/backup-files.sh
+++ source-code/conveior/bin/backup-files.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 source functions.inc.sh
 
-export PODS=$(yq e '.config.backups.files.[].name' ${CONFIG_FILE_DIR})
-export IFS=$'\n'
-for POD in $PODS;
-do
+# Fetch the list of pods to backup
+PODS=$(yq e '.config.backups.files.[].name' ${CONFIG_FILE_DIR})
+
+IFS=$'\n'
+for POD in $PODS; do
   echo_message "Backing up $POD"
 
-  export POD_PATH=$(yq e ".config.backups.files | with_entries(select(.value.name == \"$POD\")) | .[].path" ${CONFIG_FILE_DIR})
+  # Fetching paths associated with this POD
+  POD_PATHS=$(yq e ".config.backups.files[] | select(.name == \"$POD\") | .path" ${CONFIG_FILE_DIR})
 
-  export SERVER_DIR="/tmp/${POD}"
-  export FILE="${ANTI_DATE}-${POD}-${DATE}"
-  export ZIP_FILE_ONLY="${FILE}.zip"
-  export ZIP_FILE="${SERVER_DIR}/${ZIP_FILE_ONLY}"
+  for POD_PATH in $POD_PATHS; do
+    SERVER_DIR="/tmp/${POD}"
+    FILE="${ANTI_DATE}-${POD}-${DATE}"
+    ZIP_FILE_ONLY="${FILE}.zip"
+    ZIP_FILE="${SERVER_DIR}/${ZIP_FILE_ONLY}"
 
-  mkdir -p "${SERVER_DIR}"
-  find "${SERVER_DIR}" -mindepth 1 -delete
-
-  if [[ $(docker exec -i "${POD}" bash -c "cd ${POD_PATH} && ls | wc -l") != "0" ]]; then
-    docker cp ${POD}:${POD_PATH} ${SERVER_DIR}
-    export LAST_DIR=$(echo ${POD_PATH} | awk -F"/" '{print $NF}')
-    cd "${SERVER_DIR}/${LAST_DIR}" && zip -rqq "${ZIP_FILE}" "."
-
-    upload_file "${ZIP_FILE}" "backup-file/${POD}/${ZIP_FILE_ONLY}"
+    mkdir -p "${SERVER_DIR}"
     find "${SERVER_DIR}" -mindepth 1 -delete
-  fi
+
+    if [[ $(docker exec -i "${POD}" bash -c "cd ${POD_PATH} && ls | wc -l") != "0" ]]; then
+      echo "docker cp ${POD}:${POD_PATH} ${SERVER_DIR}"
+      docker cp "${POD}:${POD_PATH}" "${SERVER_DIR}"
+      LAST_DIR=$(echo ${POD_PATH} | awk -F"/" '{print $NF}')
+      cd "${SERVER_DIR}/${LAST_DIR}" && zip -rqq "${ZIP_FILE}" "."
+
+      upload_file "${ZIP_FILE}" "backup-file/${POD}/${ZIP_FILE_ONLY}"
+      find "${SERVER_DIR}" -mindepth 1 -delete
+    else
+      echo "No files to backup in ${POD_PATH} for ${POD}"
+    fi
+  done
 done
