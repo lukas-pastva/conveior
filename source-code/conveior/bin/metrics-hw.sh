a2daf2fe06643f804f0bc8c0df14345e420147c5
Lukas Pastva <lukas.pastva>
2024-05-23 12:33:53 +0200

    feat: proc

--- source-code/conveior/bin/metrics-hw.sh
+++ source-code/conveior/bin/metrics-hw.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source functions.inc.sh
 
-#heatbeat
+# heatbeat
 export EPOCH=$(date +%s)
 METRIC="conveior_hwHeartbeat ${EPOCH}"
 METRICS=$(echo -e "$METRICS\n$METRIC")
 
 # boot time
-export bootTime=$((($(date '+%s')-$(cat /proc/uptime | awk '{print $1}' | jq '.|ceil'))*1000))
+export bootTime=$((($(date '+%s') - $(cut -d. -f1 /proc/uptime))*1000))
 METRIC="conveior_hwBoot{label_name=\"boot\"} ${bootTime}"
 METRICS=$(echo -e "$METRICS\n$METRIC")
 
@@ -16,11 +16,11 @@ export totalRam=$(free | grep Mem | awk '{print $2}')
 export usedRam=$(free | grep Mem | awk '{print $3}')
 export totalRam=$(( ${totalRam} * 1024 ))
 export usedRam=$(( ${usedRam} * 1024 ))
-if (( ${totalRam} > 0 )) ; then
+if (( ${totalRam} > 0 )); then
   METRIC="conveior_hwRam{label_name=\"total\"} ${totalRam}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 fi
-if (( ${usedRam} > 0 )) ; then
+if (( ${usedRam} > 0 )); then
   METRIC="conveior_hwRam{label_name=\"used\"} ${usedRam}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 fi
@@ -30,11 +30,11 @@ export totalSwap=$(free | grep Swap | awk '{print $2}')
 export usedSwap=$(free | grep Swap | awk '{print $3}')
 totalSwap=$(( ${totalSwap} * 1024 ))
 usedSwap=$(( ${usedSwap} * 1024 ))
-if (( ${totalSwap} > 0 )) ; then
+if (( ${totalSwap} > 0 )); then
   METRIC="conveior_hwSwap{label_name=\"total\"} ${totalSwap}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 fi
-if (( ${usedSwap} > 0 )) ; then
+if (( ${usedSwap} > 0 )); then
   METRIC="conveior_hwSwap{label_name=\"used\"} ${usedSwap}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 fi
@@ -42,29 +42,27 @@ fi
 # hwCpu
 export cpu=$(vmstat 1 2 | tail -1 | awk '{print $15}')
 cpu=$(( 100 - $cpu ))
-if (( ${cpu} > 0 )) ; then
+if (( ${cpu} > 0 )); then
   METRIC="conveior_hwCpu{label_name=\"used\"} ${cpu}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
 fi
 
 # hwDisk
-while read DISK;
-do
-  export DISK_NAME=$( echo ${DISK} | grep -E [0-9] | awk '{print $1}')
-  export DISK_VALUE=$( echo ${DISK} | grep -E [0-9] | awk '{print $5}' | awk -F"%" '{print $1}')
-  if (( ${DISK_VALUE} > 0 )) ; then
+while read DISK; do
+  export DISK_NAME=$(echo ${DISK} | awk '{print $1}')
+  export DISK_VALUE=$(echo ${DISK} | awk '{print $5}' | awk -F"%" '{print $1}')
+  if (( ${DISK_VALUE} > 0 )); then
     METRIC="conveior_hwDisk{label_name=\"${DISK_NAME}\"} ${DISK_VALUE}"
     METRICS=$(echo -e "$METRICS\n$METRIC")
   fi
-done < <(df)
+done < <(df -h | grep -E '^/dev/')
 
 # docker
 CONTAINER_LIST=$(docker ps -f status=running --format="{{.Names}};{{.Size}}")
 IFS=$'\n'
-for CONTAINER in $CONTAINER_LIST;
-do
-  export CONTAINER_NAME=$( echo ${CONTAINER} | awk -F";" '{print $1}')
-  export CONTAINER_SIZE=$( echo ${CONTAINER} | awk -F";" '{print $2}' | awk -F"virtual " '{print $2}' | rev | cut -c3- | rev | numfmt --from=iec)
+for CONTAINER in $CONTAINER_LIST; do
+  export CONTAINER_NAME=$(echo ${CONTAINER} | awk -F";" '{print $1}')
+  export CONTAINER_SIZE=$(echo ${CONTAINER} | awk -F";" '{print $2}' | awk -F"virtual " '{print $2}' | rev | cut -c3- | rev | numfmt --from=iec)
 
   # hwNetwork
   export networkRx1=$(docker exec -i ${CONTAINER_NAME} cat /sys/class/net/eth0/statistics/rx_bytes)
@@ -72,21 +70,21 @@ do
   sleep 1
   export networkRx2=$(docker exec -i ${CONTAINER_NAME} cat /sys/class/net/eth0/statistics/rx_bytes)
   export networkTx2=$(docker exec -i ${CONTAINER_NAME} cat /sys/class/net/eth0/statistics/tx_bytes)
-  if [ -n "${networkRx1}" ] &&  [ -n "${networkTx1}" ] &&  [ -n "${networkRx2}" ] &&  [ -n "${networkTx2}" ] 2>/dev/null; then
+  if [ -n "${networkRx1}" ] && [ -n "${networkTx1}" ] && [ -n "${networkRx2}" ] && [ -n "${networkTx2}" ]; then
     export RX=$(( ${networkRx2} - ${networkRx1} ))
     export TX=$(( ${networkTx2} - ${networkTx1} ))
-    if (( ${RX} > 0 )) ; then
-        METRIC="conveior_hwNetwork{label_name=\"${CONTAINER_NAME}\",query_name=\"rx\"} ${RX}"
-        METRICS=$(echo -e "$METRICS\n$METRIC")
+    if (( ${RX} > 0 )); then
+      METRIC="conveior_hwNetwork{label_name=\"${CONTAINER_NAME}\",query_name=\"rx\"} ${RX}"
+      METRICS=$(echo -e "$METRICS\n$METRIC")
     fi
-    if (( ${TX} > 0 )) ; then
-        METRIC="conveior_hwNetwork{label_name=\"${CONTAINER_NAME}\",query_name=\"tx\"} ${TX}"
-        METRICS=$(echo -e "$METRICS\n$METRIC")
+    if (( ${TX} > 0 )); then
+      METRIC="conveior_hwNetwork{label_name=\"${CONTAINER_NAME}\",query_name=\"tx\"} ${TX}"
+      METRICS=$(echo -e "$METRICS\n$METRIC")
     fi
   fi
 
   # Docker size
-  if [ -n "${CONTAINER_SIZE}" ] && (( ${CONTAINER_SIZE} > 0 )) ; then
+  if [ -n "${CONTAINER_SIZE}" ] && (( ${CONTAINER_SIZE} > 0 )); then
     METRIC="conveior_hwDockerSize{label_name=\"${CONTAINER_NAME}\"} ${CONTAINER_SIZE}"
     METRICS=$(echo -e "$METRICS\n$METRIC")
   fi
@@ -111,7 +109,7 @@ do
     fi
   done
 
-  if (( THREAD_COUNT > 0 )) ; then
+  if (( THREAD_COUNT > 0 )); then
     METRIC="conveior_hwProcess{label_name=\"${CONTAINER_NAME}\"} ${THREAD_COUNT}"
     METRICS=$(echo -e "$METRICS\n$METRIC")
   fi
@@ -156,11 +154,10 @@ done
 unset IFS
 
 # hwDockerLs
-while read CONTAINER;
-do
-  export CONTAINER_NAME=$( echo ${CONTAINER} | awk -F";" '{print $1}')
-  export CONTAINER_DATE_STR=$( echo ${CONTAINER} | awk -F";" '{print $2}')
-  export CONTAINER_DATE=$(date -d ${CONTAINER_DATE_STR} +"%s" )
+while read CONTAINER; do
+  export CONTAINER_NAME=$(echo ${CONTAINER} | awk -F";" '{print $1}')
+  export CONTAINER_DATE_STR=$(echo ${CONTAINER} | awk -F";" '{print $2}')
+  export CONTAINER_DATE=$(date -d ${CONTAINER_DATE_STR} +"%s")
 
   METRIC="conveior_hwDockerLs{label_name=\"${CONTAINER_NAME}\"} ${CONTAINER_DATE}"
   METRICS=$(echo -e "$METRICS\n$METRIC")
