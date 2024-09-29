f93c68ae16d3f059ad889c658d05c266e5233929
x0661255 <lukas.pastva@o2.sk>
2024-09-29 20:06:38 +0200

    tweak

--- source-code/conveior/bin/backup-volume.sh
+++ source-code/conveior/bin/backup-volume.sh
@@ -10,7 +10,7 @@ SPLIT_SIZE="5G"
 # Create a temporary directory for backups
 mkdir -p "${BACKUP_TEMP_DIR}"
 find "${BACKUP_TEMP_DIR}" -mindepth 1 -delete
-   
+
 VOLUMES_OUTPUT=$(yq -r '.config.backups.volumes[] | "\(.name)"' "${CONFIG_FILE_DIR}")
 echo "${VOLUMES_OUTPUT}" | while read -r NAME; do
 
@@ -25,13 +25,15 @@ echo "${VOLUMES_OUTPUT}" | while read -r NAME; do
 
     # Check disk space on the host
     echo "Checking disk space..."
-    DATA_SIZE=$(docker run --rm -v "${NAME}":/data busybox sh -c "du -s /data | awk '{print \$1}'")
-    DATA_SIZE_BYTES=$((DATA_SIZE * 1024))
-    FREE_SIZE=$(df --output=avail "${BACKUP_TEMP_DIR}" | tail -1)
-    echo "Data size: ${DATA_SIZE} blocks (${DATA_SIZE_BYTES} KB)"
-    echo "Free size: ${FREE_SIZE} KB"
-    if [ "${FREE_SIZE}" -lt "${DATA_SIZE_BYTES}" ]; then
-        echo "Not enough free disk space. Required: ${DATA_SIZE_BYTES} KB, Available: ${FREE_SIZE} KB. Skipping backup for '${NAME}'."
+
+    DATA_SIZE_KB=$(docker run --rm -v "${NAME}":/data busybox sh -c "du -sk /data | awk '{print \$1}'")
+    DATA_SIZE_GB=$(awk "BEGIN {printf \"%.2f\", ${DATA_SIZE_KB}/1048576}")
+    FREE_SIZE_KB=$(df --output=avail "${BACKUP_TEMP_DIR}" | tail -1 | tr -d ' ')
+    FREE_SIZE_GB=$(awk "BEGIN {printf \"%.2f\", ${FREE_SIZE_KB}/1048576}")
+    echo "Data size: ${DATA_SIZE_GB} GB"
+    echo "Free size: ${FREE_SIZE_GB} GB"
+    if [ "${FREE_SIZE_KB}" -lt "${DATA_SIZE_KB}" ]; then
+        echo "Not enough free disk space. Required: ${DATA_SIZE_GB} GB, Available: ${FREE_SIZE_GB} GB. Skipping backup for '${NAME}'."
         continue
     fi
     echo "Sufficient disk space available."
@@ -57,7 +59,7 @@ echo "${VOLUMES_OUTPUT}" | while read -r NAME; do
     find "${SERVER_DIR}" -name "backup.tar.*" | while read -r SPLIT_FILE; do
         SPLIT_FILE_NAME=$(basename "${SPLIT_FILE}")
         echo "Uploading '${SPLIT_FILE_NAME}'..."
-        upload_file "${SPLIT_FILE}" "backup-volumes/${ANTI_DATE}-${DATE}/${SPLIT_FILE_NAME}"
+        upload_file "${SPLIT_FILE}" "backup-volumes/${NAME}/${ANTI_DATE}-${DATE}/${SPLIT_FILE_NAME}"
         echo "Uploaded '${SPLIT_FILE_NAME}' to backup storage."
         rm -f "${SPLIT_FILE}"
     done
